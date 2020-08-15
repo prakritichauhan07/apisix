f6d06322d68f17f70341afef460183328d092926
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-08-15 08:39:57 +0530

    Update linux_apisix_master_luarocks_runner.sh

--- .travis/linux_apisix_master_luarocks_runner.sh
+++ .travis/linux_apisix_master_luarocks_runner.sh
@@ -62,6 +62,7 @@ do_install() {
 script() {
     export_or_prefix
     export PATH=$OPENRESTY_PREFIX/nginx/sbin:$OPENRESTY_PREFIX/luajit/bin:$OPENRESTY_PREFIX/bin:$PATH
+    export ETCD_UNSUPPORTED_ARCH="arm64"
     openresty -V
     sudo service etcd stop
     mkdir -p ~/etcd-data
